c1123446389f38f4bc25f9c90e1000d57c8c25e5
Lukam90 <lucien.hamm@gmail.com>
2021-05-28 14:11:50 +0200

    MAJ Formulaire Session > Lieu + Formation

--- src/Data/Styles.php
+++ src/Data/Styles.php
@@ -5,6 +5,7 @@ namespace App\Data;
 abstract class Styles {
     public static $labelClass = "w3-margin-top";
     public static $inputClass = "w3-input w3-border w3-margin-bottom";
+    public static $selectClass = "w3-select w3-border w3-margin-bottom";
     public static $checkbox = "w3-check w3-border w3-margin-left w3-margin-bottom";
     public static $btnClass = "w3-button w3-indigo w3-hover-purple";
 }

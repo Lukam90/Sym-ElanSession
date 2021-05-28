617ed8568a8c257c0760aaad123163414e1d22c4
AntoineZiani <hemsa.ziani@gmail.com>
2021-05-28 09:30:42 +0200

    session controller a jour

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -16,13 +17,34 @@ class SessionController extends AbstractController
      * @Route("/add", name="session_add")
      * @Route("/{id}/edit", name="session_edit")
      */
-    public function sessionEdit()
+    public function add_edit(Session $session = null, Request $request)
     {
+        if(!$session)
+        {
+            $session = new Session();
+        }
 
+        $form = $this->createForm(SessionType::class, $session);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $session = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($session);
+            $entityManager->flush();
+            return $this->redirectToRoute('sessions_list');
+        }
+
+        return $this->render('session/add_edit.html.twig', [
+            'formSession' => $form->createView(),
+            'editMode'    => $session->getId() !== null
+        ]);
     }
 
     /**
-     ** @Route("/", name="index")
+     ** @Route("/", name="sessions_list")
      */
     public function allSession(): Response
     {
